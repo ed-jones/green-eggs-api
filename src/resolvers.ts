6edf98e2403b4543f6186450943467c5869d5c63
Ed Jones <edjones1999@gmail.com>
2021-04-13 16:03:14 +1000

    Added basic addRecipe mutation

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,12 +1,19 @@
 import { PrismaClient } from '@prisma/client'
+import { IResolvers } from 'apollo-server';
 
 const prisma = new PrismaClient();
 
-const resolvers = {
+const resolvers: IResolvers = {
   Query: {
     allRecipes: () => {
       return prisma.recipe.findMany();
     }
+  },
+  Mutation: {
+    addRecipe: async (_, {recipe}) => {
+        const createdRecipe = await prisma.recipe.create({data: recipe});
+        return {data: createdRecipe};
+      },
   }
 };
 
--- src/schema.ts
+++ src/schema.ts
@@ -1,6 +1,11 @@
 import { gql } from 'apollo-server';
 
 const typeDefs = gql`
+  fragment RecipeFragment on Recipe {
+    title
+    description
+  }
+
   type Recipe {
     title: String!
     description: String!
@@ -8,6 +13,19 @@ const typeDefs = gql`
 
   type Query {
     allRecipes: [Recipe!]!
+  }
+
+  input AddRecipeInput {
+    title: String!
+    description: String!
+  }
+
+  type AddRecipeResult {
+    data: Recipe!
+  }
+
+  type Mutation {
+    addRecipe(recipe: AddRecipeInput!): AddRecipeResult!
   }
 `;
 
