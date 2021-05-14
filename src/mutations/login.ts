ff4d2e87a61c7cb01d3a829048cb46b981a9e53b
Ed Jones <edjones1999@gmail.com>
2021-05-14 16:41:55 +1000

    Added JWT to login mutation

--- src/mutations/login.ts
+++ src/mutations/login.ts
@@ -1,7 +1,24 @@
+import jwt from 'jsonwebtoken';
+
 import { MutationLoginArgs, AuthResult } from '../generated/graphql';
 
-const login = (_: any, { loginDetails }: MutationLoginArgs): AuthResult => (
-  { data: { token: loginDetails.email } }
-);
+const secret = process.env.SECRET;
+
+const login = (_: any, { loginDetails }: MutationLoginArgs): AuthResult => {
+  if (secret) {
+    return {
+      data: {
+        token: jwt.sign(
+          { email: loginDetails.email }, secret,
+        ),
+      },
+    };
+  }
+  return {
+    error: {
+      message: 'Unable to encrypt password, secret not found',
+    },
+  };
+};
 
 export default login;
