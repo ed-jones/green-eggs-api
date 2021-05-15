329f036940887d1580e137b2c016e21d493a97d8
Ed Jones <edjones1999@gmail.com>
2021-05-15 15:06:26 +1000

    Added logic for login mutation

--- src/mutations/login.ts
+++ src/mutations/login.ts
@@ -1,22 +1,50 @@
 import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 
 import { MutationLoginArgs, AuthResult } from '../generated/graphql';
+import prisma from '../prisma';
 
 const secret = process.env.SECRET;
 
-const login = (_: any, { loginDetails }: MutationLoginArgs): AuthResult => {
-  if (secret) {
+const login = async (_: any, { loginDetails }: MutationLoginArgs): Promise<AuthResult> => {
+  const user = await prisma.user.findUnique({
+    where: {
+      email: loginDetails.email,
+    },
+  });
+
+  if (user) {
+    if (secret) {
+      bcrypt.compare(loginDetails.password, user.password,
+        (error: Error | undefined, same: boolean) => {
+          if (error) {
+            return { error };
+          }
+          if (same) {
+            return {
+              data: {
+                token: jwt.sign(
+                  { email: loginDetails.email }, secret,
+                ),
+              },
+            };
+          }
+          return {
+            error: {
+              message: 'Incorrect Password',
+            },
+          };
+        });
+    }
     return {
-      data: {
-        token: jwt.sign(
-          { email: loginDetails.email }, secret,
-        ),
+      error: {
+        message: 'Unable to encrypt password, secret not found',
       },
     };
   }
   return {
     error: {
-      message: 'Unable to encrypt password, secret not found',
+      message: 'No user with that email was found',
     },
   };
 };
