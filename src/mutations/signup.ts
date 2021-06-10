8801420fdbb743f1237356a6e884dfc69c1b5d37
Ed Jones <edjones1999@gmail.com>
2021-06-11 00:46:42 +1000

    Updated dev environment to use azure functions

--- src/mutations/signup.ts
+++ src/mutations/signup.ts
@@ -1,5 +1,5 @@
 import jwt from 'jsonwebtoken';
-import bcrypt from 'bcrypt';
+import bcrypt from 'bcryptjs';
 import { User as PrismaUser } from '@prisma/client';
 
 import { MutationSignupArgs, AuthResult } from '../generated/graphql';
