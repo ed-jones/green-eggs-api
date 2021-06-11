01a35e3c66b7c54d9b205403078d360c15986b21
Ed Jones <edjones1999@gmail.com>
2021-06-11 13:05:17 +1000

    Try this

--- src/prisma.ts
+++ src/prisma.ts
@@ -1,19 +1,4 @@
 import { PrismaClient } from '@prisma/client';
-import fs from 'fs';
-import { spawnSync } from 'child_process';
-
-if (process.env.NODE_ENV === 'production') {
-  const binaryPath = '/home/query-engine-debian-openssl-1.1.x';
-
-  if (!fs.existsSync(binaryPath)) {
-    spawnSync('cp', [
-      `${process.env.LAMBDA_TASK_ROOT}/node_modules/.prisma/client/query-engine-debian-openssl-1.1.x`,
-      '/home/',
-    ]);
-
-    spawnSync('chmod', ['555', `${process.env.LAMBDA_TASK_ROOT}/node_modules/.prisma/client/query-engine-debian-openssl-1.1.x`]);
-  }
-}
 
 const Prisma = new PrismaClient();
 export default Prisma;
