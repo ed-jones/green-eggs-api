fb2c4ac335ed1ca22d627abec3901e4573d7802a
Ed Jones <edjones1999@gmail.com>
2021-07-04 19:55:08 +1000

    Fixed recipes query

--- src/queries/recipes.ts
+++ src/queries/recipes.ts
@@ -1,5 +1,5 @@
 import prisma from '../prisma';
 
-const recipes = () => prisma.recipe.findMany({ include: { submittedBy: true } });
+const recipes = () => prisma.recipe.findMany({ include: { submittedBy: true, categories: true } });
 
 export default recipes;
