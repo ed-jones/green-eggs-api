d4ce6bdf3c327b75f9be0ed4b36a6fbe2196badf
Ed Jones <edjones1999@gmail.com>
2021-08-08 19:56:03 +1000

    Added like comment mutation

--- src/mutations/index.ts
+++ src/mutations/index.ts
@@ -5,3 +5,4 @@ export { default as likeRecipe } from './likeRecipe';
 export { default as unlikeRecipe } from './unlikeRecipe';
 export { default as addComment } from './addComment';
 export { default as replyToComment } from './replyToComment';
+export { default as likeComment } from './likeComment';
---
+++ src/mutations/likeComment.ts
@@ -0,0 +1,66 @@
+import {
+  User as PrismaUser,
+} from '@prisma/client';
+import Errors from '../errors';
+import {
+  MutationLikeCommentArgs,
+  LikeCommentResult,
+} from '../generated/graphql';
+import prisma from '../prisma';
+
+export default async (_parent: any,
+  { commentId }: MutationLikeCommentArgs,
+  context?: PrismaUser): Promise<LikeCommentResult> => {
+  try {
+    // Find user to associate with liked recipe
+    if (!context?.id) {
+      throw new Error(Errors.NO_CONTEXT);
+    }
+    const user = await prisma.user.findUnique({
+      where: {
+        id: context.id,
+      },
+    });
+    if (!user?.id) {
+      throw new Error(Errors.NO_USER);
+    }
+
+    // Find comment to like
+    const comment = await prisma.recipeComment.findUnique({
+      where: {
+        id: commentId,
+      },
+    });
+    if (!comment) {
+      throw new Error(Errors.NO_COMMENT);
+    }
+
+    const updateComment = await prisma.recipeComment.update({
+      where: {
+        id: commentId,
+      },
+      data: {
+        likedBy: {
+          connect: [
+            { id: context.id },
+          ],
+        },
+      },
+      include: {
+        likedBy: true,
+      },
+    });
+
+    if (!updateComment.likedBy.map((liker) => liker.id).includes(context.id)) {
+      throw new Error('Comment was not liked successfully');
+    }
+
+    return {};
+  } catch ({ message }) {
+    return {
+      error: {
+        message,
+      },
+    };
+  }
+};
