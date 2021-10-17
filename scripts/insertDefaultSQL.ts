828c36963337300b86bf33b6f18e5a1ae2759172
Ed Jones <edjones1999@gmail.com>
2021-10-17 16:48:57 +1100

    feat: add more fields to default sql generator

--- scripts/insertDefaultSQL.ts
+++ scripts/insertDefaultSQL.ts
@@ -3,8 +3,13 @@ const csv = require("csv-parser");
 import * as fs from "fs";
 import bcrypt from "bcryptjs";
 import prisma from "../src/prisma";
-import addRecipe from "../src/mutations/addRecipe";
-import { MutationAddRecipeArgs } from "../src/generated/graphql";
+import fullRecipeArgs from "../src/core/recipe/fullRecipeArgs";
+
+const comments = [
+  "Great recipe!",
+  "That was really cool.",
+  "I liked this one.",
+];
 
 async function arrayFromCSV<T>(filename: string): Promise<T[]> {
   return new Promise((resolve) => {
@@ -23,16 +28,6 @@ function parseArray(arrayAsString: string): string[] {
     .replace(/\"|\[|\]/g, "")
     .split(",")
     .map((s) => s.trim());
-}
-
-let users: Array<User> = [];
-
-async function getRandomUser() {
-  if (users.length === 0) {
-    users = await prisma.user.findMany();
-  }
-
-  return users[Math.floor(Math.random() * (users.length - 1))];
 }
 
 // Create Diets
@@ -65,15 +60,18 @@ arrayFromCSV<Prisma.CategoryCreateManyInput>("./csv/categories.csv").then(
 // Create Users
 arrayFromCSV<Prisma.UserCreateInput>("./csv/users.csv")
   .then((data) => {
-    data.forEach(async (user) => {
+    return data.map(async (user) => {
       const salt = await bcrypt.genSalt(10);
       const hash = await bcrypt.hash(user.password, salt);
-      await prisma.user.create({
+      return await prisma.user.create({
         data: { ...user, password: hash },
       });
     });
   })
-  .then(() => {
+  .then((users) => {
+    async function getRandomUser() {
+      return users[Math.floor(Math.random() * (users.length - 1))];
+    }
     // Create Recipes
     arrayFromCSV<any>("./csv/recipes.csv").then(async (data) => {
       data.forEach(async (datum) => {
@@ -81,11 +79,37 @@ arrayFromCSV<Prisma.UserCreateInput>("./csv/users.csv")
           await prisma.recipe.create({
             data: {
               ...datum,
+              servingCount: Math.floor(Math.random() * 8) + 1,
+              timeEstimate: new Date(
+                (Math.floor(Math.random() * 8) + 1) * 1000 * 60 * 60 * 15
+              ),
+              likedBy: {
+                connect: (
+                  await Promise.all(
+                    users.map(async () => ({
+                      id: (await getRandomUser()).id,
+                    }))
+                  )
+                ).slice(0, Math.floor(Math.random() * (users.length - 1))),
+              },
               submittedBy: {
                 connect: {
                   id: (await getRandomUser()).id,
                 },
               },
+              recipeComments: {
+                create: {
+                  author: {
+                    connect: {
+                      id: (await getRandomUser()).id,
+                    },
+                  },
+                  contents:
+                    comments[Math.floor(Math.random() * (comments.length - 1))],
+                },
+              },
+              description:
+                "Et nemo officiis consequatur. Esse assumenda voluptatem laudantium ipsam alias autem dolore unde. Nostrum tenetur libero nihil libero. Quos repellendus ullam at.\nProvident non doloremque et excepturi ut. Reprehenderit accusantium eius in. Ab dicta amet doloribus rerum exercitationem. Odit non optio repudiandae quas. Commodi eos et quas sint minima molestiae quod.",
               ingredients: {
                 create: parseArray(datum.ingredients).map((name) => ({
                   genericIngredient: {
@@ -115,6 +139,7 @@ arrayFromCSV<Prisma.UserCreateInput>("./csv/users.csv")
                 })),
               },
             },
+            ...fullRecipeArgs,
           })
         );
       });
